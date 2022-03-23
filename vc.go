20ab54145052ca24d872f5af6f2db498f9233ea2
Peiyang Qiao <qiaopeiyang@hyperchain.cn>
2022-03-23 18:59:54 +0800

    feat(vc.go): update vc.go

--- vc.go
+++ vc.go
@@ -3,8 +3,24 @@ package crypto
 //MethodName plugin function name
 const MethodName = "NewSDK"
 
+//Logger interface
+type Logger interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	Notice(v ...interface{})
+	Noticef(format string, v ...interface{})
+	Warning(v ...interface{})
+	Warningf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+	Critical(v ...interface{})
+	Criticalf(format string, v ...interface{})
+}
+
 //NewSDKFunc plugin function type
-type NewSDKFunc func(path, user, namespace string) (ChainSDK, error)
+type NewSDKFunc func(path, user, namespace string, logger Logger) (ChainSDK, error)
 
 //EventType type EventType
 type EventType int
